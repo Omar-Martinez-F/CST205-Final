633b67e6930defe8b050b131125e6b015217a97e
Omar-Martinez-F <omartinezfuentes@csumb.edu>
2026-04-20 13:20:10 -0700

    Added files for better structure

---
+++ ui/main_window.py
@@ -0,0 +1,4 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QComboBox
+# Build main ui here 
+# Firt ui implention should be simple dropdown,buttons ect
+# To make this project more unique we should add more features to mess with audio files later
